e8e64afec62901fc7bddc8d32991550eec3f0e4b
Addis Rofik Erlangga <rofikaddis@gmail.com>
2025-04-22 07:15:45 +0700

    import React

--- app/index.tsx
+++ app/index.tsx
@@ -1,15 +1,28 @@
-import { Text, View } from "react-native";
+import React, { Component } from 'react';
+import { View, Text, StatusBar } from 'react-native';
 
-export default function Index() {
-  return (
-    <View
-      style={{
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-      }}
-    >
-      <Text>Edit app/index.tsx to edit this screen.</Text>
-    </View>
-  );
+class Index extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      text: ''
+    };
+  }
+
+  render() {
+    return (
+      <View style={{ flex: 1 }}>
+        <StatusBar backgroundColor="#0288d1" barStyle="light-content" />
+
+        <View style={{ padding: 20, backgroundColor: '#03a9f4', elevation: 1 }}>
+          <Text style={{ textAlign: 'center', color: '#FFFFFF', fontWeight: 'bold', fontSize: 18 }}>
+            MyKamus
+          </Text>
+        </View>
+      </View>
+    );
+  }
 }
+
+export default Index;
+
