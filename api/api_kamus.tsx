e09463d674379e2457f983da4f83a54b94aa94a1
Addis Rofik Erlangga <rofikaddis@gmail.com>
2025-04-23 19:56:05 +0700

    pemanggilan api

---
+++ api/api_kamus.tsx
@@ -0,0 +1,15 @@
+const fetchDictionaryData = async ({ word }: { word?: string }) => {
+  try {
+    const response = await fetch(
+      `https://api.dictionaryapi.dev/api/v2/entries/en/${word}`
+    );
+    if(!response.ok) {
+        throw new Error(`Error: ${response.status}`)
+    }
+
+    const data = await response.json();
+    return data;
+  } catch (error) {
+    console.log(error);
+  }
+};
