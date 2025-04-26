860174cbc8e9051f42f50f1d53c5019bc2b979e4
Addis Rofik Erlangga <rofikaddis@gmail.com>
2025-04-26 22:27:17 +0700

    tambah gambar sebagai foto profile

--- app/(tabs)/profile.tsx
+++ app/(tabs)/profile.tsx
@@ -1,9 +1,23 @@
-import { View, Text } from "react-native";
+import { View, Text, Image, StyleSheet } from "react-native";
 
-export default function VerbLayout(){
-    return (
-        <View>
-            <Text>Profile page</Text>
-        </View>
-    )
+export default function VerbLayout() {
+  return (
+    <View style={styles.container}>
+        <Text style={{fontSize:30, fontWeight: "600", marginBottom: 30}}>Profile</Text>
+      <Image style={styles.img} source={require("@/assets/images/profile.jpg")} />
+    </View>
+  );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex : 1,
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  img : {
+    width: 200,
+    height : 200,
+    marginBottom : 450,
+  }
+});
