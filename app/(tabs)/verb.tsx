6781c799a72a302ca40fd3a5284355713019646c
Addis Rofik Erlangga <rofikaddis@gmail.com>
2025-04-26 22:29:27 +0700

    menebalkan title

--- app/(tabs)/verb.tsx
+++ app/(tabs)/verb.tsx
@@ -22,7 +22,7 @@ const styles = StyleSheet.create({
     },
     title: {
         fontSize: 24,
-        fontWeight: "bold",
+        fontWeight: "600",
         marginBottom: 10,
     },
     description: {
